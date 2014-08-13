6cecd48fa2c0d543591f1bcce5a05d0c46e73908
wildcatsoft <wildcatsoft@msn.com>
2014-08-13 23:28:41 +0800

    Add more default bindings

--- src/defaultBind.js
+++ src/defaultBind.js
@@ -1,21 +1,29 @@
 ﻿var WinJS;
 (function (WinJS) {
     (function (KO) {
+        var _koBindings = {
+            "visible": visibleBind,
+            "text": textBind,
+            "html": htmlBind,
+            "if": ifBind,
+            "ifnot": ifNotBind,
+            "click": clickBind,
+            "submit": submitBind,
+            "enable": enableBind,
+            "disable": disableBind,
+            "hasFocus": hasFocusBind,
+            "value": valueBind,
+            "checked": checkedBind
+        };
+
         KO.defaultBind = WinJS.Binding.initializer(function (source, sourceProps, dest, destProps) {
             var isHandled = false;
             if (destProps || destProps.length == 1) {
                 var destProp = destProps[0];
 
-                switch (destProp) {
-                    case "visible":
-                        isHandled = visibleBind(source, sourceProps, dest, destProp);
-                        break;
-                    case "text":
-                        isHandled = textBind(source, sourceProps, dest, destProp);
-                        break;
-                    case "hasFocus":
-                        isHandled = hasFocusBind(source, sourceProps, dest, destProp);
-                        break;
+                if (_koBindings[destProp]) {
+                    _koBindings[destProp](source, sourceProps, dest);
+                    isHandled = true;
                 }
             }
 
@@ -24,61 +32,163 @@
             }
         });
 
-        function visibleBind(source, sourceProps, dest, destProp) {
+        function visibleBind(source, sourceProps, dest) {
             var converter = WinJS.Binding.converter(function (visible) {
                 return visible ? "" : "none";
             });
 
             converter(source, sourceProps, dest, ["style", "display"]);
-
-            return true;
         }
 
-        function textBind(source, sourceProps, dest, destProp) {
+        function textBind(source, sourceProps, dest) {
             WinJS.Binding.defaultBind(source, sourceProps, dest, ["textContent"]);
-            return true;
         }
 
-        function hasFocusBind(source, sourceProps, dest, destProp) {
-            var pendingInitalFocusEvent = 0;
-            var pendingIntialBlurEvent = 0;
+        function htmlBind(source, sourceProps, dest) {
+            WinJS.Binding.defaultBind(source, sourceProps, dest, ["innerHtml"]);
+        }
 
-            function onFocus() {
-                if (pendingInitalFocusEvent > 0) {
-                    pendingInitalFocusEvent--;
-                } else {
-                    _nestedSet(source, sourceProps, true);
+        function _ifBindConverter(dest, value) {
+            var destElement = dest;
+            var children = [];
+
+            if (value && children.length > 0) {
+                for (var child in children) {
+                    dest.appendChild(child);
+                }
+                children = [];
+            } else if (!value && destElement.hasChildNodes()) {
+                for (var child in destElement.children) {
+                    children.push(child);
+                }
+                while (destElement.hasChildNodes()) {
+                    destElement.removeChild(destElement.lastChild);
+                }
+            }
+            return value;
+        }
+        ;
+
+        function ifBind(source, sourceProps, dest) {
+            var converter = WinJS.Binding.converter(function (value) {
+                return _ifBindConverter(dest, value);
+            });
+
+            converter(source, sourceProps, dest, ["_winjs_ko_if"]);
+        }
+
+        function ifNotBind(source, sourceProps, dest) {
+            var converter = WinJS.Binding.converter(function (value) {
+                return _ifBindConverter(dest, !value);
+            });
+
+            converter(source, sourceProps, dest, ["_winjs_ko_ifnot"]);
+        }
+
+        function clickBind(source, sourceProps, dest) {
+            WinJS.Binding.defaultBind(source, sourceProps, dest, ["onclick"]);
+        }
+
+        function eventBind(source, sourceProps, dest) {
+            function _foreachEvent(events, func) {
+                events = events || {};
+                for (var key in events) {
+                    var event = events[key];
+                    if (typeof event == "function") {
+                        func(key, event);
+                    }
                 }
             }
 
-            function onBlur() {
-                if (pendingIntialBlurEvent > 0) {
-                    pendingIntialBlurEvent--;
-                } else {
-                    _nestedSet(source, sourceProps, false);
+            var converter = WinJS.Binding.converter(function (events) {
+                _foreachEvent(dest["_winjs_ko_eventBind"], function (key, event) {
+                    dest.removeEventListener(key, event);
+                });
+
+                _foreachEvent(events, function (key, event) {
+                    dest.addEventListener(key, event);
+                });
+            });
+
+            converter(source, sourceProps, dest, ["_winjs_ko_eventBind"]);
+        }
+
+        function submitBind(source, sourceProps, dest) {
+            var submitEvent;
+
+            dest.addEventListener("submit", function (ev) {
+                if (submitEvent && !submitEvent(ev)) {
+                    ev.preventDefault();
                 }
+            });
+
+            var converter = WinJS.Binding.converter(function (event) {
+                submitEvent = (typeof (event) == "function") ? event : null;
+                return submitEvent;
+            });
+
+            converter(source, sourceProps, dest, ["_winjs_ko_submitBind"]);
+        }
+
+        function _enableOrDisableBind(source, sourceProps, dest, isDisabled) {
+            var converter = WinJS.Binding.converter(function (value) {
+                var disabled = isDisabled ? value : !value;
+                return disabled ? "true" : null;
+            });
+
+            converter(source, sourceProps, dest, ["disabled"]);
+        }
+
+        function enableBind(source, sourceProps, dest) {
+            _enableOrDisableBind(source, sourceProps, dest, false);
+        }
+
+        function disableBind(source, sourceProps, dest) {
+            _enableOrDisableBind(source, sourceProps, dest, true);
+        }
+
+        function valueBind(source, sourceProps, dest) {
+            WinJS.Binding.defaultBind(source, sourceProps, dest, ["value"]);
+
+            if (_isObservable(source)) {
+                dest.onchange = function () {
+                    _nestedSet(source, sourceProps, dest.value);
+                };
             }
+        }
 
-            var isSourceObservable = _isObservable(source);
-
-            if (isSourceObservable) {
-                dest.onfocus = onFocus;
-                dest.onblur = onBlur;
+        function hasFocusBind(source, sourceProps, dest) {
+            function elementHasFocus(element) {
+                return element.ownerDocument.activeElement == element;
             }
 
             var converter = WinJS.Binding.converter(function (hasFocus) {
-                hasFocus ? dest.focus() : dest.blur();
+                var destHasFocus = elementHasFocus(dest);
 
-                if (isSourceObservable) {
-                    hasFocus ? pendingInitalFocusEvent++ : pendingIntialBlurEvent++;
+                if (hasFocus != destHasFocus) {
+                    hasFocus ? dest.focus() : dest.blur();
+                }
+
+                if (_isObservable(source)) {
+                    dest.onfocus = dest.onblur = function () {
+                        _nestedSet(source, sourceProps, elementHasFocus(dest));
+                    };
                 }
 
                 return hasFocus;
             });
 
-            converter(source, sourceProps, dest, ["hasFocus"]);
+            converter(source, sourceProps, dest, ["_winjs_ko_hasFocus"]);
+        }
 
-            return true;
+        function checkedBind(source, sourceProps, dest) {
+            WinJS.Binding.defaultBind(source, sourceProps, dest, ["checked"]);
+
+            if (_isObservable(source)) {
+                dest.onchange = function () {
+                    _nestedSet(source, sourceProps, dest.checked);
+                };
+            }
         }
 
         function _isObservable(data) {
--- src/defaultBind.ts
+++ src/defaultBind.ts
@@ -1,5 +1,20 @@
 ﻿module WinJS.KO {
 
+    var _koBindings = {
+        "visible": visibleBind,
+        "text": textBind,
+        "html": htmlBind,
+        "if": ifBind,
+        "ifnot": ifNotBind,
+        "click": clickBind,
+        "submit": submitBind,
+        "enable": enableBind,
+        "disable": disableBind,
+        "hasFocus": hasFocusBind,
+        "value": valueBind,
+        "checked" : checkedBind,
+    }
+
     export var defaultBind = WinJS.Binding.initializer(function (
         source, sourceProps : string[], dest : HTMLElement, destProps: string[]) {
 
@@ -7,83 +22,179 @@
         if (destProps || destProps.length == 1) {
             var destProp = destProps[0];
 
-            switch (destProp) {
-                case "visible": 
-                    isHandled = visibleBind(source, sourceProps, dest, destProp);
-                    break;
-                case "text":
-                    isHandled = textBind(source, sourceProps, dest, destProp);
-                    break;
-                case "hasFocus":
-                    isHandled = hasFocusBind(source, sourceProps, dest, destProp);
-                    break;
-            }
+            if (_koBindings[destProp]) {
+                _koBindings[destProp](source, sourceProps, dest);
+                isHandled = true;
+            }   
         }
 
-        if (!isHandled) {
-            WinJS.Binding.defaultBind(source, sourceProps, dest, destProps);
+        if (!isHandled){
+            WinJS.Binding.defaultBind(source, sourceProps, dest, destProps)
         }
-
     });
 
-    function visibleBind(source, sourceProps: string[], dest : HTMLElement, destProp): boolean {
+    function visibleBind(source, sourceProps: string[], dest : HTMLElement) {
         var converter = WinJS.Binding.converter(function (visible: boolean) {
             return visible? "" : "none";
         });
 
         converter(source, sourceProps, dest, ["style", "display"]);
-
-        return true;
     }
 
-    function textBind(source, sourceProps: string[], dest: HTMLElement, destProp): boolean {
+    function textBind(source, sourceProps: string[], dest: HTMLElement) {
         WinJS.Binding.defaultBind(source, sourceProps, dest, ["textContent"]);
-        return true;
     }
 
-    function hasFocusBind(source, sourceProps: string[], dest: HTMLElement, destProp): boolean {
-        var pendingInitalFocusEvent = 0;
-        var pendingIntialBlurEvent = 0;
+    function htmlBind(source, sourceProps: string[], dest: HTMLElement) {
+        WinJS.Binding.defaultBind(source, sourceProps, dest, ["innerHtml"]);
+    }
 
-        function onFocus() {
-            if (pendingInitalFocusEvent > 0) {
-                pendingInitalFocusEvent--
+    function _ifBindConverter(dest: HTMLElement, value) {
+        var destElement = dest;
+        var children = [];
+
+        if (value && children.length > 0) {
+            for (var child in children) {
+                dest.appendChild(child);
+            }
+            children = [];
+        }
+        else if (!value && destElement.hasChildNodes()) {
+            for (var child in destElement.children) {
+                children.push(child);
+            }
+            while (destElement.hasChildNodes()) {
+                destElement.removeChild(destElement.lastChild);
+            }
+        }
+        return value;
+    };
+
+    function ifBind(source, sourceProps: string[], dest: HTMLElement) {
+        var converter = WinJS.Binding.converter(function (value) {
+            return _ifBindConverter(dest, value);
+        });
+
+        converter(source, sourceProps, dest, ["_winjs_ko_if"]);
+    }
+
+    function ifNotBind(source, sourceProps: string[], dest: HTMLElement) {
+        var converter = WinJS.Binding.converter(function (value) {
+            return _ifBindConverter(dest, !value);
+        });
+
+        converter(source, sourceProps, dest, ["_winjs_ko_ifnot"]);
+    }
+
+    function clickBind(source, sourceProps: string[], dest: HTMLElement) {
+        WinJS.Binding.defaultBind(source, sourceProps, dest, ["onclick"]);
+    }
+
+    function eventBind(source, sourceProps: string[], dest: HTMLElement) {
+        function _foreachEvent(events, func: Function) {
+            events = events || {};
+            for (var key in events) {
+                var event = events[key];
+                if (typeof event == "function") {
+                    func(key, event);
                 }
-            else {
-                _nestedSet(source, sourceProps, true);
             }
         }
 
-        function onBlur() {
-            if (pendingIntialBlurEvent > 0) {
-                pendingIntialBlurEvent--
-                }
-            else {
-                _nestedSet(source, sourceProps, false);
+        var converter = WinJS.Binding.converter(function (events) {
+            _foreachEvent(dest["_winjs_ko_eventBind"], function (key, event) {
+                dest.removeEventListener(key, event);
+            });
+
+            _foreachEvent(events, function (key, event) {
+                dest.addEventListener(key, event);
+            });
+
+        });
+
+        converter(source, sourceProps, dest, ["_winjs_ko_eventBind"]);
+    }
+
+    function submitBind(source, sourceProps: string[], dest: HTMLElement) {
+
+        var submitEvent: Function;
+
+        dest.addEventListener("submit", function (ev: Event) {
+            if (submitEvent && !submitEvent(ev)) {
+                ev.preventDefault();
+            }
+        });
+
+        var converter = WinJS.Binding.converter(function (event) {
+            submitEvent = (typeof (event) == "function") ? event : null;
+            return submitEvent;
+        });
+
+        converter(source, sourceProps, dest, ["_winjs_ko_submitBind"]);
+    }
+
+    function _enableOrDisableBind(source, sourceProps: string[], dest: HTMLElement, isDisabled: boolean) {
+
+        var converter = WinJS.Binding.converter(function (value) {
+            var disabled = isDisabled ? value : !value;
+            return disabled ? "true" : null;
+        });
+
+        converter(source, sourceProps, dest, ["disabled"]);
+    }
+
+    function enableBind(source, sourceProps: string[], dest: HTMLElement) {
+        _enableOrDisableBind(source, sourceProps, dest, false);
+    }
+
+    function disableBind(source, sourceProps: string[], dest: HTMLElement) {
+        _enableOrDisableBind(source, sourceProps, dest, true);
+    }
+
+    function valueBind(source, sourceProps: string[], dest: HTMLInputElement) {
+        WinJS.Binding.defaultBind(source, sourceProps, dest, ["value"]);
+
+        if (_isObservable(source)) {
+            dest.onchange = function () {
+                _nestedSet(source, sourceProps, dest.value);
             }
         }
+    }
 
-        var isSourceObservable = _isObservable(source)
+    function hasFocusBind(source, sourceProps: string[], dest: HTMLElement) {
 
-        if (isSourceObservable) {
-            dest.onfocus = onFocus;
-            dest.onblur = onBlur;
+        function elementHasFocus(element: HTMLElement): boolean {
+            return element.ownerDocument.activeElement == element;
         }
 
         var converter = WinJS.Binding.converter(function (hasFocus: boolean) {
 
-            hasFocus ? dest.focus() : dest.blur();
+            var destHasFocus = elementHasFocus(dest);
 
-            if (isSourceObservable) {
-                hasFocus ? pendingInitalFocusEvent++ : pendingIntialBlurEvent++;
+            if (hasFocus != destHasFocus) {
+                hasFocus ? dest.focus() : dest.blur();
+            }
+
+            if (_isObservable(source)) {
+                dest.onfocus = dest.onblur = function () {
+                    _nestedSet(source, sourceProps, elementHasFocus(dest));
+                }
             }
 
             return hasFocus;
         });
 
-        converter(source, sourceProps, dest, ["hasFocus"]);
+        converter(source, sourceProps, dest, ["_winjs_ko_hasFocus"]);
+    }
 
-        return true;
+    function checkedBind(source, sourceProps: string[], dest: HTMLInputElement) {
+        WinJS.Binding.defaultBind(source, sourceProps, dest, ["checked"]);
+
+        if (_isObservable(source)) {
+            dest.onchange = function () {
+                _nestedSet(source, sourceProps, dest.checked);
+            }
+        }
     }
 
     function _isObservable(data): boolean {
